6ff628f272c00ef6f50a6f4155d26c71cf23bc67
Ilija Radovanovic <ilija009@gmail.com>
2023-06-22 11:41:56 +0200

    testing mui and social icons

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import './Footer.css'
+import SocialIcon from './SocialIcon'
 
 export default function Footer() {
     const year = new Date().getFullYear()
 
     return (
         <footer className='Footer'>
+            <SocialIcon />
             <div>
                 <p>&copy; <span className="copyright-year">{year}</span> Ilija Radovanović</p>
                 <p>Made with &hearts; & &#9749; in Novi Sad, Budva and Belgrade.</p>
