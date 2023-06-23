6fa4a62a86c5804d42b8edfe3e5da1c14f467506
Ilija Radovanovic <ilija009@gmail.com>
2023-06-23 12:30:43 +0200

    update social links

--- react/src/components/SocialIcon.jsx
+++ react/src/components/SocialIcon.jsx
@@ -7,10 +7,10 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 export default function SocialIcon() {
   return (
     <div className='SocialIcon'>
-      <a href="#" target='_blank'>
+      <a href="https://www.instagram.com/dzidzabidzice/" target='_blank'>
         <InstagramIcon />
       </a>
-      <a href="#" target='_blank'>
+      <a href="https://www.facebook.com/dzidzabidzice021/" target='_blank'>
         <FacebookIcon />
       </a>
     </div>
